cca38aa1443a30c0f17aaf33166da8314cdf77a4
ashuashenafi <ashuashenafi@gmail.com>
2020-06-02 16:39:58 -0400

    display detail about currently auth. user

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-"#Spring Boot Example 404 Using Database-based Authentication" 
+##Spring Boot Example 405 Persisting Current User Information
+Learning Objectives
+* Storing User Information when Authentication
+* Retrieving information about the current user
--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,10 +1,17 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    UserRepository userRepository;
 
     @RequestMapping("/")
     public String index() {
@@ -24,5 +31,12 @@ public class HomeController {
     @RequestMapping("/admin")
     public String admin() {
         return "admin";
+    }
+
+    @RequestMapping("/secure")
+    public String secure(Principal principal, Model model) {
+        String username = principal.getName();
+        model.addAttribute("user", userRepository.findByUsername(username));
+        return "secure";
     }
 }
